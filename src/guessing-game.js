3d0446d23cacea2eaa42bf72520bce42c6ec390e
annaAzh <forarre@yandex.ru>
2023-09-02 22:50:48 +0300

    feat: solved task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        while (this.min < this.max) {
+            this.middle = Math.round((this.min + this.max) / 2);
+            return this.middle;
+        }
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
